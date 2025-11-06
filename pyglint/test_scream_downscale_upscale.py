0ef52ceb1090084c0bdbb083d3d6c91cfe5adb38
ggslc <stephen.l.cornford@gmail.com>
2025-11-06 16:17:48 +0000

    tidying up figures

--- pyglint/test_scream_downscale_upscale.py
+++ pyglint/test_scream_downscale_upscale.py
@@ -17,21 +17,21 @@ from transformation import cell_id
 X, Y, XX, YY, xXY, yXY, x, y, xx, yy, \
        local_to_global_map, sftc_g, topo_g, usrf, \
            mask,topo_max_ec = \
-               scream_surface_test_data(local_grid_shape = (480,512),
-                                        global_grid_shape = (18,16),
-                                        global_min_elev = 100.0,
+               scream_surface_test_data(local_grid_shape = (512,511),
+                                        global_grid_shape = (31,33),
+                                        global_min_elev = 50.0,
                                         global_max_elev = 1700, 
-                                        global_grid_angle=np.pi/2,
+                                        global_grid_angle=np.pi/6,
                                         n_elev = 5)
   
 
-#n = 0
+n = 0
 #usrf = usrf*0 + 0.5*(topo_max_ec[n] + topo_max_ec[n+1]) 
 #mask = mask*0 + 1
 #sftc_g = sftc_g*0 + 5
   
 #interpolate global fields for each elevation class
-sftc_l = interp_to_local(sftc_g, xXY, yXY, xx, yy, order=0)
+sftc_l = interp_to_local(sftc_g, xXY, yXY, xx, yy, order=1)
 topo_l = interp_to_local(topo_g, xXY, yXY, xx, yy, order=0)
 
 #project onto surface
@@ -72,114 +72,85 @@ class TestUpscale(unittest.TestCase):
 unittest.main()
 
 
-if True:
+def temp_color_map(ax, xc, yc, T,cmap='plasma'):
+    
+    ax.set_aspect('equal')
+    pc = ax.pcolormesh(xc, yc, T,  vmin=-10,vmax=10,cmap=cmap)
 
-    
-    fig, axs = plt.subplots(4,3,figsize=(8,10))    
-    fig.suptitle('Downscaling', fontsize=16)
-    jndx = 0
-    
-    def temp_color_map(ax, x, y, T):
-        return ax.pcolormesh(x, y, T,  vmin=-10,vmax=10,cmap='bwr')
-    
-    for NEC in [0,1,2,3,4]:
-        #pc = axs.flat[jndx].pcolormesh(X, Y, sftc_g[:,:,NEC], vmin=-1,vmax=1,cmap='bwr_r')
-        #fig.colorbar(pc,ax=axs.flat[jndx])
-        #jndx += 1
-        
-        pc = temp_color_map(axs.flat[jndx], xXY, yXY, sftc_g[:,:,NEC])
-        #fig.colorbar(pc,ax=axs.flat[jndx])
-        axs.flat[jndx].axhline(np.min(y),color='k',lw=0.5)
-        axs.flat[jndx].axhline(np.max(y),color='k',lw=0.5)
-        axs.flat[jndx].axvline(np.min(x),color='k',lw=0.5)
-        axs.flat[jndx].axvline(np.max(x),color='k',lw=0.5)
-        axs.flat[jndx].set_title(f'global/coarse, ec = {NEC}')
-        axs.flat[jndx].set_xticks([])
-        axs.flat[jndx].set_yticks([])
-        jndx += 1
-        
-    for NEC in [0,1,2,3,4]:      
-        pc = temp_color_map(axs.flat[jndx], x, y, sftc_l[:,:,NEC])
-        #fig.colorbar(pc,ax=axs.flat[jndx])
-        axs.flat[jndx].set_title(f'local/fine, ec = {NEC}')
-        axs.flat[jndx].set_xticks([])
-        axs.flat[jndx].set_yticks([])
-        jndx += 1
-    
-    pc = temp_color_map(axs.flat[jndx], x, y, sftc)
-    #fig.colorbar(pc,ax=axs.flat[jndx])
-    #axs.flat[jndx].contour(x,y,usrf,[0,500,1000,1500],colors=['k'])
-    axs.flat[jndx].set_title('local/fine, at surface ')
-    axs.flat[jndx].set_xticks([])
-    axs.flat[jndx].set_yticks([])
-    jndx += 1
-    
-    axs.flat[jndx].plot(topo_g.flat[:]-20.0, sftc_g.flat,'.',label='sftc_g',ms=1)
-    axs.flat[jndx].plot(topo_l.flat[::77]+20, sftc_l.flat[::77],'.',label='sftc_l',ms=1)
-    axs.flat[jndx].plot(usrf.flat[::77],sftc.flat[::77],'.',label='sftc',color='k',ms=1)
-    axs.flat[jndx].legend()
-    jndx += 1
+    return pc
+
  
     
-# %% Upscaling figure
-    fig, axs = plt.subplots(4, 3, figsize=(8,10))
-    fig.suptitle('Upscaling', fontsize=16)
+# %% Downscaling figure
+if True:
+    
+    fig = plt.figure(figsize=(8,11))    
+    fig.suptitle('Downscaling', fontsize=14)
+    
+    margin = 0.025
+    aspect = fig.get_figwidth()/fig.get_figheight()
+    step = 2
     nec = p_sftc_g.shape[2]
-    jndx = 0
+    nec_range = range(0, nec, step)
+    width = (1.0 - (1+len(nec_range))*margin)/len(nec_range)
+    height = width*aspect
+    for i,ec in enumerate(range(0, nec, step)):
+        ax = fig.add_axes(((1+i)*margin + i*width, 1 - (3*margin+height), width,height))
+        pc = temp_color_map(ax, xXY, yXY, sftc_g[:,:,ec])
+        _ = [ax.axhline(yl,color='k',lw=0.5) for yl in [np.min(y),np.max(y)]]
+        _ = [ax.axvline(xl,color='k',lw=0.5) for xl in [np.min(x),np.max(x)]]
+        ax.set_title(f'global, {topo_max_ec[ec]} < s < {topo_max_ec[ec+1]}')
+        ax.set_xticks([])
+        if (ec > 0 ):
+            ax.set_yticks([])
+        
+        ax = fig.add_axes(((1+i)*margin + i*width, 1 - (5*margin+2*height), width,height))
+        pc = temp_color_map(ax, x, y, sftc_l[:,:,ec])
+        ax.set_title(f'local, {topo_max_ec[ec]} < s < {topo_max_ec[ec+1]}')
+        if (ec > 0 ):
+            ax.set_yticks([])
+            
+    #large, tsfc on surface
+    ax = fig.add_axes((margin, margin, 1.75*width,1.75*height))
+    pc = temp_color_map(ax, x, y, sftc)
+    fig.colorbar(pc,ax=ax,shrink = 0.5, pad=margin)
+    ax.set_title('local/fine, at surface ')   
     
+    #large, distribution
+    ax = fig.add_axes((4*margin + 1.75*width, 2.25*margin, 2*margin+width,1.4*height))
+    ax.plot(sftc_g.flat,topo_g.flat[:]-20.0, 's',label='sftc_g (z+20m)',ms=2)
+    ax.plot(sftc_l.flat[::77], topo_l.flat[::77]+20,'^',label='sftc_l (z-20m)',ms=2)
+    ax.plot(sftc.flat[::77],usrf.flat[::77],'o',label='sftc',color='k',ms=2)
+    ax.set_yticks(topo_max_ec,topo_max_ec/1000 )
+    ax.grid()
+    ax.set_xlabel(r'$T$ ($^\circ$C)')
+    ax.set_ylabel(r'$z$ (km)')
+    ax.legend(loc='upper right')
     
-    M, N = sftc_g.shape[0:2]
-    J = np.arange(0,M)
-    I = np.arange(0,N)
-    II, JJ = np.meshgrid(I, J)
-    CC = cell_id(II, JJ, N, M)
-    
+            
+# %% Upscaling figure
+if True:
+  
+    fig, axs = plt.subplots(3, 3, figsize=(8,11)) 
+    fig.suptitle('Upscaling', fontsize=14)
+     
     replace_where = np.where(np.isfinite(p_sftc_g),p_sftc_g, sftc_g)
-    err = replace_where-sftc_g
-    
-    
-    
-    for ec in range(0, nec):
-        
-        pc = temp_color_map(axs.flat[jndx], xXY , yXY,  p_sftc_g[:, :, ec])
-        axs.flat[jndx].set_title(f'{topo_max_ec[ec]} < s < {topo_max_ec[ec+1]}')
-        axs.flat[jndx].set_xticks([])
-        axs.flat[jndx].set_yticks([])
-        axs.flat[jndx].axhline(np.min(y),color='k',lw=0.5)
-        axs.flat[jndx].axhline(np.max(y),color='k',lw=0.5)
-        axs.flat[jndx].axvline(np.min(x),color='k',lw=0.5)
-        axs.flat[jndx].axvline(np.max(x),color='k',lw=0.5) 
-        axs.flat[jndx].contour(xXY , yXY, II,I+0.5,linewidths=0.5)
-        axs.flat[jndx].contour(xXY , yXY, JJ,J+0.5,linewidths=0.5)
-        axs.flat[jndx].set_xlim(-300,300)
-        axs.flat[jndx].set_ylim(-300,300)
-       
-        
-        
-        jndx += 1
-        
-      
-        pc = temp_color_map(axs.flat[jndx], xXY , yXY, err[:,:,ec])
-        axs.flat[jndx].contour(xXY , yXY, II,I+0.5,linewidths=0.5)
-        axs.flat[jndx].contour(xXY , yXY, JJ,J+0.5,linewidths=0.5)
-        axs.flat[jndx].set_xlim(-300,300)
-        axs.flat[jndx].set_ylim(-300,300)
-        #axs.flat[jndx].set_ylim(-250,250)
-        
-        axs.flat[jndx].set_title(f'{topo_max_ec[ec]} < s < {topo_max_ec[ec+1]}')
-        axs.flat[jndx].set_xticks([])
-        axs.flat[jndx].set_yticks([])
-        axs.flat[jndx].axhline(np.min(y),color='k',lw=0.5)
-        axs.flat[jndx].axhline(np.max(y),color='k',lw=0.5)
-        axs.flat[jndx].axvline(np.min(x),color='k',lw=0.5)
-        axs.flat[jndx].axvline(np.max(x),color='k',lw=0.5)
-        
-        
-        #ax.contour(usrf_g[:, :, ec], [topo_max_ec[ec], topo_max_ec[ec+1]])
-        #fig.colorbar(pc)
-        jndx += 1
-    
-    #sftc_vsum = np.sum(p_sftc_g[:, :, :], axis=2)
-    #pc = axs.flat[jndx].pcolormesh(sftc_vsum, vmin=-1, vmax=1, cmap='bwr_r')
-    #fig.colorbar(pc)
+    err = np.where(np.isfinite(p_sftc_g),p_sftc_g-sftc_g, 0.0)
+    for i,ec in enumerate(range(0, nec, step)):
+        for j, ffn in  enumerate(zip([sftc_g,p_sftc_g,err],['global','upscaled','error'])):
+           ax = axs[j,i]
+           f, fn = ffn
+           pc = temp_color_map(ax, xXY, yXY, f[:,:,ec],cmap='bwr')
+           _ = [ax.axhline(yl,color='k',lw=0.5) for yl in [np.min(y),np.max(y)]]
+           _ = [ax.axvline(xl,color='k',lw=0.5) for xl in [np.min(x),np.max(x)]]
+           ax.set_title(f'{fn}, ec = {ec}')
+           ax.set_xticks([])
+           fig.colorbar(pc,ax=ax,orientation='horizontal')
+           if (ec > 0 ):
+               ax.set_yticks([])
+           
+         
+
+
+
 
