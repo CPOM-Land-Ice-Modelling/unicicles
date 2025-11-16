9395e4f44b87480fe98a7b32d117810e56ef1894
ggslc <stephen.l.cornford@gmail.com>
2025-11-16 11:53:40 +0000

    linting

--- pyglint/coupling.py
+++ pyglint/coupling.py
@@ -10,16 +10,18 @@ from downscale import interp_to_local, interp_to_surface
 from transformation import local_to_global_map, cell_id
 
 
-def glint_conservation_adjust(smb_ism_nc, mask_ism, grid_ism,
-                              area_atm, grid_atm, valid_atm,
+def glint_conservation_adjust(f2d_ism_nc, mask_ism, grid_ism,
+                              f3d_atm, area_atm, grid_atm, valid_atm,
                               down_transform):
+
+
 
     # conservation enforcement - see glint_downscaling_gcm
     # roughly - adjust smb so that the total smb_ism within an
     # atmosphere grid box equals the smb_atm integrated vertically.
     # this resembles the glint code. blocky.
     # there is a NASTY! comment in the glint code
-    smb_ism = np.zeros(smb_ism_nc.shape) + smb_ism_nc
+    f2d_ism = np.zeros(f2d_ism_nc.shape) + f2d_ism_nc
 
     ism_to_atm_map = local_to_global_map(down_transform, grid_atm, grid_ism)
 
@@ -31,21 +33,21 @@ def glint_conservation_adjust(smb_ism_nc, mask_ism, grid_ism,
             if np.any(valid_atm[:, J, I]):
 
                 cc = cell_id(I, J, *grid_atm.shape)
-                
+
                 # vertical sum
-                atm_col_total = np.sum(smb_atm[:, J, I]*area_atm[:, J, I])
-    
+                atm_col_total = np.sum(f3d_atm[:, J, I]*area_atm[:, J, I])
+
                 # horizontal sum
-                ism_total = dx**2 * np.sum(np.where(ism_to_atm_map == cc, smb_ism, 0.0))
+                ism_total = dx**2 * np.sum(np.where(ism_to_atm_map == cc, f2d_ism, 0.0))
                 ism_area =  dx**2 * np.sum(np.where(ism_to_atm_map == cc, 1.0, 0.0))
 
                 if ism_area > 0:
                     smb_adjust[I, J] = (atm_col_total - ism_total)/ism_area
-                    smb_ism = np.where(ism_to_atm_map == cc,
-                                       smb_ism + smb_adjust[I, J],
-                                       smb_ism)
+                    f2d_ism = np.where(ism_to_atm_map == cc,
+                                       f2d_ism + smb_adjust[I, J],
+                                       f2d_ism)
 
-    return smb_ism * mask_ism
+    return f2d_ism * mask_ism
 
 
 def atm_to_ism(smb_atm, sfct_atm, topo_atm, area_atm,  grid_atm,
@@ -70,10 +72,10 @@ def atm_to_ism(smb_atm, sfct_atm, topo_atm, area_atm,  grid_atm,
         (mask_ism*interp_to_surface(f_xyz, topo_xyz, topo_ism)
          for f_xyz in (sfct_xyz, smb_xyz))
 
-    if not type(area_atm) is None:
+    if not isinstance(area_atm, type(None)):
         smb_ism = glint_conservation_adjust(
             smb_ism, mask_ism, grid_ism,
-            area_atm, grid_atm, valid_atm,
+            smb_atm, area_atm, grid_atm, valid_atm,
             down_transform)
 
     return smb_ism, sfct_ism
@@ -97,16 +99,16 @@ if __name__ == "__main__":
     up_tr, down_tr = up_down_pair(PROJ_ARCTIC_4326)
 
     #example ice sheet model output (input to the coupler). bisicles CF
-    nc_ism_in = Dataset('bike_cf_gris4326.nc','r')
-    topo_ism = nc_ism_in['orog'][:,:]
-    frac_ism = nc_ism_in['sftgif'][:,:]
-    grid_ism = Uniform2DGrid(nc_ism_in['x'][:].data, nc_ism_in['y'][:].data)
+    nc_bike_in = Dataset('bike_cf_gris4326.nc','r')
+    topo_bike = nc_bike_in['orog'][:,:]
+    frac_bike = nc_bike_in['sftgif'][:,:]
+    grid_bike = Uniform2DGrid(nc_bike_in['x'][:].data, nc_bike_in['y'][:].data)
 
 
     #example atmosphere model output (input to the coupler).
-    nc_atm_in = Dataset('atmos_cx209c_P1Y_20000101-20010101_icecouple.nc','r')
-    lon_ism, lat_ism = up_tr.global_XY(grid_ism.coords)
-    #plt.pcolormesh(lon_ism, lat_ism,topo_ism,vmin=0,vmax=2000, cmap='jet')
+    nc_um_in = Dataset('atmos_cx209c_P1Y_20000101-20010101_icecouple.nc','r')
+    lon_bike, lat_bike = up_tr.global_XY(grid_bike.coords)
+    #plt.pcolormesh(lon_bike, lat_bike,topo_bike,vmin=0,vmax=2000, cmap='jet')
 
 
     ilo, ihi = 148, 188
@@ -115,27 +117,29 @@ if __name__ == "__main__":
 
     #ilo, ihi = 0, 192
     #jlo, jhi = 0, 144
-    grid_atm = Uniform2DGrid(nc_atm_in['longitude'][ilo:ihi].data,
-                                    nc_atm_in['latitude'][jlo:jhi].data)
+    grid_um = Uniform2DGrid(nc_um_in['longitude'][ilo:ihi].data,
+                                    nc_um_in['latitude'][jlo:jhi].data)
 
-    prep_atm = lambda arr: np.transpose(arr[:,jlo:jhi,ilo:ihi], axes=[0,1,2])
+    def prep_um(arr):
+        return arr[:,jlo:jhi,ilo:ihi]
 
-    area_atm = prep_atm(nc_atm_in['tile_surface_area'])
-    smb_atm = prep_atm(nc_atm_in['ice_smb']) * 910 * 12 # values suggest kg/month
-    sfct_atm = prep_atm(nc_atm_in['ice_stemp'])
+    area_um = prep_um(nc_um_in['tile_surface_area'])
+    smb_um = prep_um(nc_um_in['ice_smb']) * 910 * 12 # values suggest kg/month
+    sfct_um = prep_um(nc_um_in['ice_stemp'])
+    z_id = nc_um_in['tile_id']
 
-    lon, lat = grid_atm.coords
+    lon, lat = grid_um.coords
     nlat, nlon = lat.shape
-    nec =  10
-    topo_atm = np.zeros(smb_atm.shape)
+    nec = z_id.shape[0]
+    topo_um = np.zeros(smb_um.shape)
     #making this up too
     topo_mid = np.array([31.86, 297.01, 551.87, 846.16, 1151.70,
                 1457.07, 1808.83, 2257.02,  2737.89, 3099.39])
     for ec in range(0,nec):
-        topo_atm[ec,:,:] = topo_mid[ec]
+        topo_um[ec,:,:] = topo_mid[ec]
 
 
-    plt.pcolormesh(*grid_atm.axes, np.mean(sfct_atm, axis=0),vmin=-50, cmap='bwr_r')
+    plt.pcolormesh(*grid_um.axes, np.mean(sfct_um, axis=0),vmin=-50, cmap='bwr_r')
     plt.colorbar()
     plt.show()
 
@@ -144,32 +148,35 @@ if __name__ == "__main__":
 
 
 #%% downscaling & upscaling
-    smb_ism, sfct_ism = atm_to_ism(smb_atm, sfct_atm, topo_atm, area_atm,
-                                   grid_atm, topo_ism, frac_ism,
-                                   grid_ism, up_tr, down_tr)
+    smb_bike, sfct_bike = atm_to_ism(smb_um, sfct_um, topo_um, area_um,
+                                   grid_um, topo_bike, frac_bike,
+                                   grid_bike, up_tr, down_tr)
 # %% plot
-    if True:
+    plot = True
+    if plot:
 
         def cf(ax, z, zl, label=True):
-            cs = ax.contour(*km(*grid_ism.axes), z, zl, colors=['k'] ,linewidths=0.5, linestyles='-')
+            cs = ax.contour(*km(*grid_bike.axes), z, zl, colors=['k'] ,linewidths=0.5, linestyles='-')
             if label:
                 ax.clabel(cs, cs.levels, fontsize=8)
 
+        def km(x,y):
+            return (x*1.0e-3,y*1e-3)
+
         fig, axs = plt.subplots(1,2)
-        [ax.set_aspect('equal') for ax in axs.flat]
-        km = lambda x,y : (x*1.0e-3,y*1e-3)
+        __ = [ax.set_aspect('equal') for ax in axs.flat]
         #a = 0.1
         ax = axs.flat[0]
-        pc = ax.pcolormesh(*km(*grid_ism.axes),smb_ism,cmap='bwr_r',vmin=-1,vmax=1)
+        pc = ax.pcolormesh(*km(*grid_bike.axes),smb_bike,cmap='bwr_r',vmin=-1,vmax=1)
         fig.colorbar(pc,ax=ax,shrink=0.5)
-        cf(ax, lon_ism, 360+np.arange(-60,-15,5))
-        cf(ax, lat_ism, np.arange(60,90,5))
-        cf(ax, frac_ism, [0.05], label=False)
+        cf(ax, lon_bike, 360+np.arange(-60,-15,5))
+        cf(ax, lat_bike, np.arange(60,90,5))
+        cf(ax, frac_bike, [0.05], label=False)
 
         ax = axs.flat[1]
-        pc = ax.pcolormesh(*km(*grid_ism.axes),sfct_ism,cmap='plasma',vmin=-50,vmax=0)
+        pc = ax.pcolormesh(*km(*grid_bike.axes),sfct_bike,cmap='plasma',vmin=-50,vmax=0)
         fig.colorbar(pc,ax=ax,shrink=0.5)
-        cf(ax, lon_ism, 360+np.arange(-60,-15,5))
-        cf(ax, lat_ism, np.arange(60,90,5))
-        cf(ax, frac_ism, [0.05], label=False)
+        cf(ax, lon_bike, 360+np.arange(-60,-15,5))
+        cf(ax, lat_bike, np.arange(60,90,5))
+        cf(ax, frac_bike, [0.05], label=False)
 
--- pyglint/upscale.py
+++ pyglint/upscale.py
@@ -7,7 +7,6 @@ Created on Thu Oct 30 16:13:44 2025
 """
 
 import numpy as np
-from functools import partial
 from transformation import cell_id, missing
 
 
@@ -15,8 +14,8 @@ def subset_ec(topo, topo_max, ec):
     return (topo < topo_max[ec+1]) & (topo >= topo_max[ec])
 
 
-def local_to_global_cell_mec(field, topo, mask, topo_max, local_to_global_map,
-                            lcolfrac, cell_indx):
+def local_to_global_cell_agg(field, topo, mask, topo_max, local_to_global_map,
+                            cell_indx):
 
     indx = mask & (local_to_global_map  == cell_indx)
 
@@ -25,29 +24,29 @@ def local_to_global_cell_mec(field, topo, mask, topo_max, local_to_global_map,
     field_count = np.zeros(nec)
     ts = topo[indx]
     tf = field[indx]
-    field_count_col = len(tf) 
+    field_count_col = len(tf)
     for ec in range(0, nec):
         kndx = subset_ec(ts, topo_max, ec)
         t = tf[kndx]
         field_sum[ec] = np.sum(t)
-        field_count[ec] = len(t) 
+        field_count[ec] = len(t)
 
     return field_sum, field_count, field_count_col
 
 
 def mean_to_global_mec(field, topo, mask, topo_max, local_to_global_map,
-                       global_shape, lcolfrac=False, missing = missing):
+                       global_shape, lcolfrac=False, missing_val = missing):
     """
-    
+
     Compute the means of 2D field on the global 3D grid.
-    Cell values will be non-zero where the 2D surface elevation (topo) 
+    Cell values will be non-zero where the 2D surface elevation (topo)
     intersects the cell. Cell vertical boundaries are defined
     by topo_max
-    
+
     Parameters
     ----------
     field : numpy.ndarray / float 2D array
-        field on the local grid 
+        field on the local grid
     topo : numpy.ndarray / float 2D array
         surface elevation on the local grid
     topo_max : numpy.ndarray / float 1D array
@@ -57,7 +56,7 @@ def mean_to_global_mec(field, topo, mask, topo_max, local_to_global_map,
     global_shape : (int, int, int)
         shape of the global grid
     lcolfrac : bool, optional
-        If true, mean computed over global column, if false mean 
+        If true, mean computed over global column, if false mean
         computef over the gobal cell. The default is False.
 
     Returns
@@ -67,21 +66,24 @@ def mean_to_global_mec(field, topo, mask, topo_max, local_to_global_map,
 
     """
 
-    __, nJ, nI = global_shape
-    global_field = np.full(global_shape, missing) 
+    __, nj_global, ni_global = global_shape
+    global_field = np.full(global_shape, missing_val)
+    
+    def cellid(i_global,j_global):
+        return cell_id(i_global,j_global,ni_global,nj_global)
 
-    for I in range(0, nI):
-        for J in range(0, nJ):
-            fsum, count, count_col  = local_to_global_cell_mec( \
+    for i in range(0, ni_global):
+        for j in range(0, nj_global):
+            fsum, count, count_col  = local_to_global_cell_agg( \
                     field, topo, mask, topo_max, \
                     local_to_global_map,  \
-                    lcolfrac, cell_id(I,J,nI,nJ))
-    
+                    cellid(i,j))
+
             if lcolfrac:
                 if count_col > 0:
-                    global_field[:, J, I] = fsum/count_col
-            else: 
+                    global_field[:, j, i] = fsum/count_col
+            else:
                 valid = count > 0
-                global_field[valid, J, I] = fsum[valid]/count[valid]
-            
+                global_field[valid, j, i] = fsum[valid]/count[valid]
+
     return global_field
