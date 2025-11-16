2684b5407792ddf98cf39e21c95fb90265a921bb
ggslc <stephen.l.cornford@gmail.com>
2025-11-16 16:13:01 +0000

    linting

--- pyglint/downscale.py
+++ pyglint/downscale.py
@@ -8,43 +8,39 @@ Created on Mon Nov  3 15:50:48 2025
 
 import numpy as np
 from scipy.interpolate import LinearNDInterpolator, NearestNDInterpolator
-from scipy.interpolate import RegularGridInterpolator
 
-def interp_to_local(field_g, global_coords, local_coords, order=1, valid=None):
+
+def interp_to_local(zg, global_coords, local_coords, order=1, valid=None):
 
     xg, yg = global_coords
     xl, yl = local_coords
 
-    nec = field_g.shape[0] 
-    nyl, nxl = xl.shape 
     if xl.shape != yl.shape:
-        raise('xl.shape != yl.shape')
-    field_l = np.empty((nec,nyl,nxl))
-    
-    if not (order == 1 or order == 0):
-        raise('order != 0 or 1')
-    
+        raise ValueError('xl.shape != yl.shape')
+
+    zl = np.empty((zg.shape[0],*xl.shape))
+
+    if order not in (0,1):
+        raise ValueError('order not in (0,1)')
+
     interpf = LinearNDInterpolator if order == 1 else NearestNDInterpolator
-    
-    for ec in range(0, nec):
 
-        z =  field_g[ec,:,:]      
+    for ec in range(0, zg.shape[0]):
 
-        if type(valid) is np.ndarray:
+        z = zg[ec,:,:]
+
+        if isinstance(valid, np.ndarray):
             # fill the null regions with nearest-neigbours
             v = valid[ec,:,:]
-            zf = NearestNDInterpolator(
-                np.array([xg[v].flat, yg[v].flat]).T, z[v].flat)
-            z = zf(xg,yg)
-                
-        field_l[ec,:,:] = interpf(np.array([xg.flat, yg.flat]).T, z.flat)(xl,yl)
-        
-        
-        
-    return field_l
+            z = NearestNDInterpolator(
+                np.array([xg[v].flat, yg[v].flat]).T, z[v].flat) (xg,yg)
+
+        zl[ec,:,:] = interpf(np.array([xg.flat, yg.flat]).T, z.flat)(xl,yl)
+
+    return zl
 
 def interp_to_surface(field_l, topo_l, topo, lapse=0.0):
-    
+
     field = np.zeros(topo.shape)
     nec = topo_l.shape[0]
 
@@ -53,15 +49,15 @@ def interp_to_surface(field_l, topo_l, topo, lapse=0.0):
         wlo = np.where((topo >= topo_l[ec-1,:,:]) & (topo < topo_l[ec,:,:]),w,0)
         wup = np.where((topo >= topo_l[ec-1,:,:]) & (topo < topo_l[ec,:,:]),1-w,0)
         field += wup * field_l[ec,:,:] + wlo * field_l[ec-1,:,:]
-        
-    
-    field = np.where(topo < topo_l[0,:,:], 
-                     field_l[0,:,:] + lapse*(topo_l[0,:,:] - topo), 
+
+
+    field = np.where(topo < topo_l[0,:,:],
+                     field_l[0,:,:] + lapse*(topo_l[0,:,:] - topo),
                      field )
 
-    field = np.where(topo > topo_l[-1,:,:], 
-                     field_l[-1,:,:] + lapse*(topo_l[-1,:,:] - topo), 
+    field = np.where(topo > topo_l[-1,:,:],
+                     field_l[-1,:,:] + lapse*(topo_l[-1,:,:] - topo),
                      field )
-    
-        
+
+
     return field
